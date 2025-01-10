ec3d9e2100f9a6cbe0c39d45f9ee02da590e3b57
Louis Boulagnon <louis@la-quincaillerie.fr>
2025-01-10 23:41:46 +0100

    Add helper functions

---
+++ assets/functions/qce.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Qce {
+	if ( ! class_exists( Kernel::class ) && trait_exists( \Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait::class ) ) {
+		class Kernel extends \Symfony\Component\HttpKernel\Kernel {
+			use \Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
+
+			private string $project_dir;
+
+			public function getProjectDir(): string {
+				return $this->project_dir ??= get_stylesheet_directory();
+			}
+		}
+	}
+}
+
+namespace {
+	if ( ! class_exists( \Qce\Kernel::class ) ) {
+		return;
+	}
+
+	if ( ! function_exists( 'qce_kernel' ) ) {
+		function qce_kernel( string $env = null, bool $debug = null ) {
+			static $kernel;
+
+			return $kernel ??= new \Qce\Kernel(
+				match ( $env ??= \wp_get_environment_type() ) {
+					'production' => 'prod',
+					'local', 'staging', 'development' => 'dev',
+					default => $env,
+				},
+				$debug ?? WP_DEBUG
+			);
+		}
+	}
+
+	if ( ! function_exists( 'qce' ) ) {
+		function qce( ?string $id = null ) {
+			$container = qce_kernel()->getContainer();
+
+			return $id ? $container->get( $id ) : $container;
+		}
+	}
+}
